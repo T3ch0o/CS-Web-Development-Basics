c22cb51d8bfec03bd435d48532dfbdb02ee500a4
Aleksbgbg <kozichka01@abv.bg>
2018-10-21 20:25:32 +0100

    [Results] Add several results classes, which inherit from HttpResponse.

---
+++ Web Server/WebServer/Results/HtmlResult.cs
@@ -0,0 +1,13 @@
+﻿namespace WebServer.Results
+{
+    using Http.Models;
+    using Http.Models.Responses;
+
+    public class HtmlResult : HttpResponse
+    {
+        public HtmlResult(HttpStatusCode statusCode) : base(statusCode)
+        {
+            Headers.Add("Content-Type", "text/html");
+        }
+    }
+}
---
+++ Web Server/WebServer/Results/RedirectResult.cs
@@ -0,0 +1,13 @@
+﻿namespace WebServer.Results
+{
+    using Http.Models;
+    using Http.Models.Responses;
+
+    public class RedirectResult : HttpResponse
+    {
+        public RedirectResult(string location) : base(HttpStatusCode.SeeOther)
+        {
+            Headers.Add("Location", location);
+        }
+    }
+}
