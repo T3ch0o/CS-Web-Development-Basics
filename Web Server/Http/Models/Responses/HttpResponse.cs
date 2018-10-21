98f2d9094d76b2f81036b4d16972745d899e573d
Aleksbgbg <kozichka01@abv.bg>
2018-10-21 19:38:28 +0100

    [HttpResponse] Add HTTP response model.

---
+++ Web Server/Http/Models/Responses/HttpResponse.cs
@@ -0,0 +1,43 @@
+﻿namespace Http.Models.Responses
+{
+    using System;
+    using System.Text;
+
+    using Http.Configuration;
+    using Http.Extensions;
+    using Http.Models.Headers;
+
+    public class HttpResponse : IHttpResponse
+    {
+        public HttpResponse(HttpStatusCode statusCode)
+        {
+            StatusCode = statusCode;
+        }
+
+        public HttpStatusCode StatusCode { get; }
+
+        public IHttpHeaderCollection Headers { get; } = new HttpHeaderCollection();
+
+        public byte[] BodyBytes { get; set; }
+
+        public byte[] FormResponseBytes()
+        {
+            byte[] headBytes = Encoding.ASCII.GetBytes(ToString());
+
+            byte[] responseBytes = new byte[headBytes.Length + BodyBytes.Length];
+
+            headBytes.CopyTo(responseBytes, 0);
+            BodyBytes.CopyTo(responseBytes, headBytes.Length);
+
+            Buffer.BlockCopy(headBytes, 0, responseBytes, 0, headBytes.Length);
+            Buffer.BlockCopy(BodyBytes, 0, responseBytes, headBytes.Length, BodyBytes.Length);
+
+            return responseBytes;
+        }
+
+        public override string ToString()
+        {
+            return $"{Constants.HttpProtocolVersion} {StatusCode.ToHttpFormat()}\r\n{Headers}\r\n\r\n";
+        }
+    }
+}
---
+++ Web Server/Http/Models/Responses/IHttpResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Http.Models.Responses
+{
+    using Http.Models.Headers;
+
+    public interface IHttpResponse
+    {
+        HttpStatusCode StatusCode { get; }
+
+        IHttpHeaderCollection Headers { get; }
+
+        byte[] BodyBytes { get; set; }
+
+        byte[] FormResponseBytes();
+    }
+}
