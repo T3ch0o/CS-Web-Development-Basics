729665f3f9481300672463cb759869908c90da39
Aleksbgbg <kozichka01@abv.bg>
2018-10-21 14:22:31 +0100

    [StringExtensions] Add capitalize extension, which returns a string with its first character capitalized.

---
+++ Web Server/Http/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Http.Extensions
+{
+    internal static class StringExtensions
+    {
+        internal static string Capitalize(this string value)
+        {
+            if (value.Length == 0)
+            {
+                return value;
+            }
+
+            char[] characters = value.ToCharArray();
+
+            {
+                ref char firstCharacter = ref characters[0];
+                firstCharacter = char.ToUpper(firstCharacter);
+            }
+
+            return new string(characters);
+        }
+    }
+}
