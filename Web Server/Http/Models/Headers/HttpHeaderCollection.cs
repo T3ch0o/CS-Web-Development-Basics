36730994435eb87040f0be1bbcd8c8bf3532e57b
Aleksbgbg <kozichka01@abv.bg>
2018-10-21 15:05:41 +0100

    [HttpHeaderCollection] Add a collection class which holds a dictionary of headers.

---
+++ Web Server/Http/Models/Headers/HttpHeaderCollection.cs
@@ -0,0 +1,36 @@
+﻿namespace Http.Models.Headers
+{
+    using System.Collections.Generic;
+
+    public class HttpHeaderCollection : IHttpHeaderCollection
+    {
+        private readonly Dictionary<string, string> _headers = new Dictionary<string, string>();
+
+        public void Add(string name, string value)
+        {
+            _headers.Add(name, value);
+        }
+
+        public void Add(HttpHeader header)
+        {
+            Add(header.Name, header.Value);
+        }
+
+        public bool ContainsHeader(string name)
+        {
+            return _headers.ContainsKey(name);
+        }
+
+        public bool TryGetHeader(string name, out HttpHeader? header)
+        {
+            if (_headers.TryGetValue(name, out string value))
+            {
+                header = new HttpHeader(name, value);
+                return true;
+            }
+
+            header = null;
+            return false;
+        }
+    }
+}
---
+++ Web Server/Http/Models/Headers/IHttpHeaderCollection.cs
@@ -0,0 +1,13 @@
+﻿namespace Http.Models.Headers
+{
+    public interface IHttpHeaderCollection
+    {
+        void Add(string name, string value);
+
+        void Add(HttpHeader header);
+
+        bool ContainsHeader(string name);
+
+        bool TryGetHeader(string name, out HttpHeader? header);
+    }
+}
