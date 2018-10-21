dc8eca8191abcf62638b14ed265dcb2d22b420c1
Aleksbgbg <kozichka01@abv.bg>
2018-10-21 14:03:18 +0100

    [HttpStatusCodeExtensions] Add ToHttpFormat method, converting a status code to its header equivalent.

---
+++ Web Server/Http/Extensions/HttpStatusCodeExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Http.Extensions
+{
+    using Http.Models;
+
+    public static class HttpStatusCodeExtensions
+    {
+        public static string ToHttpFormat(this HttpStatusCode httpStatusCode)
+        {
+            return string.Concat((int)httpStatusCode, " ", httpStatusCode);
+        }
+    }
+}
