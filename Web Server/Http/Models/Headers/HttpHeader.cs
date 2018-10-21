d68342b7ae7b9d5e7b37f9330b0ded90386d9f0f
Aleksbgbg <kozichka01@abv.bg>
2018-10-21 14:26:42 +0100

    [HttpHeader] Add a name-value class for http headers.

---
+++ Web Server/Http/Models/Headers/HttpHeader.cs
@@ -0,0 +1,24 @@
+﻿namespace Http.Models.Headers
+{
+    public class HttpHeader
+    {
+        public HttpHeader()
+        {
+        }
+
+        public HttpHeader(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Value}";
+        }
+    }
+}
