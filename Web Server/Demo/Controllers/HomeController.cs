f54b05bc5c97e1c3ec6b127b2aa5c4e00981aed1
Aleksbgbg <kozichka01@abv.bg>
2018-10-23 15:30:22 +0100

    [Demo] Add demo server project which uses a controller to return a 'Hello, World!' HTML string.

---
+++ Web Server/Demo/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo.Controllers
+{
+    using Http.Models;
+    using Http.Models.Responses;
+
+    using WebServer.Results;
+
+    public class HomeController
+    {
+        public IHttpResponse Index()
+        {
+            return new HtmlResult("<h1>Hello World!</h1>", HttpStatusCode.OK);
+        }
+    }
+}
---
+++ Web Server/Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Demo
+{
+    using System.Threading.Tasks;
+
+    using Demo.Controllers;
+
+    using Http.Models;
+
+    using WebServer;
+    using WebServer.Routing;
+
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            ServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+
+            serverRoutingTable.RegisterOrOverwriteRoute(HttpRequestMethod.GET, "/", request => new HomeController().Index());
+
+            Server server = new Server(5000, serverRoutingTable);
+
+            await server.Run();
+        }
+    }
+}
