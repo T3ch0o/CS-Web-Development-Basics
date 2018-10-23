2c0f851670ebfb89a7df85ba1c17fb0724ae531e
Aleksbgbg <kozichka01@abv.bg>
2018-10-23 12:08:44 +0100

    [ConnectionHandler] Add class which manages socket connection, reading from and responding to a HTTP request sent over sockets.

---
+++ Web Server/WebServer/ConnectionHandler.cs
@@ -0,0 +1,83 @@
+﻿namespace WebServer
+{
+    using System;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Http.Models;
+    using Http.Models.Requests;
+    using Http.Models.Responses;
+
+    using WebServer.Routing;
+
+    public class ConnectionHandler
+    {
+        private const int ChunkSize = 4096;
+
+        private readonly Socket _clientSocket;
+
+        private readonly ServerRoutingTable _serverRoutingTable;
+
+        public ConnectionHandler(Socket clientSocket, ServerRoutingTable serverRoutingTable)
+        {
+            _clientSocket = clientSocket;
+            _serverRoutingTable = serverRoutingTable;
+        }
+
+        public async Task ProcessRequestAsync()
+        {
+            Task<IHttpRequest> requestTask = ReadRequest();
+
+            IHttpRequest request = await requestTask;
+
+            IHttpResponse response;
+
+            if (requestTask.IsFaulted)
+            {
+                if (requestTask.Exception.InnerException is ArgumentException)
+                {
+                    response = new HttpResponse(HttpStatusCode.BadRequest);
+                }
+                else
+                {
+                    response = new HttpResponse(HttpStatusCode.InternalServerError);
+                }
+            }
+            else
+            {
+                response = _serverRoutingTable.HandleRequest(request);
+            }
+
+            await SendResponse(response);
+
+            _clientSocket.Dispose();
+        }
+
+        private async Task<IHttpRequest> ReadRequest()
+        {
+            Memory<byte> readBuffer = new Memory<byte>(new byte[ChunkSize]);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while (true)
+            {
+                int bytesRead = await _clientSocket.ReceiveAsync(readBuffer, SocketFlags.None);
+
+                stringBuilder.Append(Encoding.ASCII.GetString(readBuffer.Span.Slice(0, bytesRead)));
+
+                if (bytesRead < ChunkSize)
+                {
+                    break;
+                }
+            }
+
+            return HttpRequest.Parse(stringBuilder.ToString());
+        }
+
+        private async Task SendResponse(IHttpResponse response)
+        {
+            Memory<byte> responseData = new Memory<byte>(response.FormResponseBytes());
+            await _clientSocket.SendAsync(responseData, SocketFlags.None);
+        }
+    }
+}
