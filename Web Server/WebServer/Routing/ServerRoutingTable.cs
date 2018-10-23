f5c9dba5f4af3ffa634f21eb283ca9af6445a6f9
Aleksbgbg <kozichka01@abv.bg>
2018-10-23 11:14:42 +0100

    [ServerRoutingTable] Add class which holdsa table of route functions, depending on HTTP method and path.

---
+++ Web Server/WebServer/Routing/ServerRoutingTable.cs
@@ -0,0 +1,26 @@
+﻿namespace WebServer.Routing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Http.Models;
+    using Http.Models.Requests;
+    using Http.Models.Responses;
+
+    public class ServerRoutingTable
+    {
+        public ServerRoutingTable()
+        {
+            Routes = new Dictionary<HttpRequestMethod, Dictionary<string, Func<IHttpRequest, IHttpResponse>>>();
+
+            foreach (HttpRequestMethod httpRequestMethod in Enum.GetValues(typeof(HttpRequestMethod))
+                                                                .Cast<HttpRequestMethod>())
+            {
+                Routes[httpRequestMethod] = new Dictionary<string, Func<IHttpRequest, IHttpResponse>>();
+            }
+        }
+
+        public Dictionary<HttpRequestMethod, Dictionary<string, Func<IHttpRequest, IHttpResponse>>> Routes { get; }
+    }
+}
