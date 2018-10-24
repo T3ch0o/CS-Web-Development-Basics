3417d1fea87351ab425971de5d1673bab7a61f13
Aleksbgbg <kozichka01@abv.bg>
2018-10-24 11:12:10 +0100

    [HttpCookieCollection] Add a collection of cookies class.

---
+++ Web Server/Http/Models/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,34 @@
+﻿namespace Http.Models.Cookies
+{
+    using System.Collections.Generic;
+
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        private readonly Dictionary<string, HttpCookie> _cookies = new Dictionary<string, HttpCookie>();
+
+        public void Add(HttpCookie cookie)
+        {
+            _cookies[cookie.Name] = cookie;
+        }
+
+        public bool Contains(string name)
+        {
+            return _cookies.ContainsKey(name);
+        }
+
+        public HttpCookie GetCookie(string name)
+        {
+            return _cookies[name];
+        }
+
+        public bool HasCookies()
+        {
+            return _cookies.Count > 0;
+        }
+
+        public override string ToString()
+        {
+            return string.Join("; ", _cookies.Values);
+        }
+    }
+}
---
+++ Web Server/Http/Models/Cookies/IHttpCookieCollection.cs
@@ -0,0 +1,13 @@
+﻿namespace Http.Models.Cookies
+{
+    public interface IHttpCookieCollection
+    {
+        void Add(HttpCookie cookie);
+
+        bool Contains(string name);
+
+        HttpCookie GetCookie(string name);
+
+        bool HasCookies();
+    }
+}
