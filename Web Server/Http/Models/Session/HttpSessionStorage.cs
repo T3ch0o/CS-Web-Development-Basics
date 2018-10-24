281e53bf3bb633842a3aceb0611d0f7a62aa6826
Aleksbgbg <kozichka01@abv.bg>
2018-10-24 17:04:06 +0100

    [HttpSessionStorage] Create static container for a dictionary of session objects.

---
+++ Web Server/Http/Models/Session/HttpSessionStorage.cs
@@ -0,0 +1,16 @@
+﻿namespace Http.Models.Session
+{
+    using System.Collections.Concurrent;
+
+    public static class HttpSessionStorage
+    {
+        public const string SessionCookieKey = "SIS_ID";
+
+        private static readonly ConcurrentDictionary<string, IHttpSession> Sessions = new ConcurrentDictionary<string, IHttpSession>();
+
+        public static IHttpSession GetSession(string id)
+        {
+            return Sessions.GetOrAdd(id, key => new HttpSession(key));
+        }
+    }
+}
