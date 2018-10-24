97bc08fbbf96cebaf7fb858da0254f7f31f8d0c1
Aleksbgbg <kozichka01@abv.bg>
2018-10-24 16:59:20 +0100

    [HttpSession] Implement session class.

---
+++ Web Server/Http/Models/Session/HttpSession.cs
@@ -0,0 +1,57 @@
+﻿namespace Http.Models.Session
+{
+    using System.Collections.Generic;
+
+    public class HttpSession : IHttpSession
+    {
+        private readonly Dictionary<string, object> _parameters = new Dictionary<string, object>();
+
+        public HttpSession(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; }
+
+        public void AddParameter<T>(string name, T parameter)
+        {
+            _parameters.Add(name, parameter);
+        }
+
+        public void ClearParameters()
+        {
+            _parameters.Clear();
+        }
+
+        public bool ContainsParameter(string name)
+        {
+            return _parameters.ContainsKey(name);
+        }
+
+        public object GetParameter(string name)
+        {
+            return _parameters[name];
+        }
+
+        public T GetParameter<T>(string name)
+        {
+            return (T)_parameters[name];
+        }
+
+        public bool TryGetParameter(string name, out object parameter)
+        {
+            bool exists = _parameters.TryGetValue(name, out object value);
+
+            parameter = value;
+            return exists;
+        }
+
+        public bool TryGetParameter<T>(string name, out T parameter)
+        {
+            bool exists = _parameters.TryGetValue(name, out object value);
+
+            parameter = (T)value;
+            return exists;
+        }
+    }
+}
---
+++ Web Server/Http/Models/Session/IHttpSession.cs
@@ -0,0 +1,21 @@
+﻿namespace Http.Models.Session
+{
+    public interface IHttpSession
+    {
+        string Id { get; }
+
+        void AddParameter<T>(string name, T parameter);
+
+        void ClearParameters();
+
+        bool ContainsParameter(string name);
+
+        object GetParameter(string name);
+
+        T GetParameter<T>(string name);
+
+        bool TryGetParameter(string name, out object parameter);
+
+        bool TryGetParameter<T>(string name, out T parameter);
+    }
+}
