9df908e8d10ace5635a59f70e4844d5d4770830e
Aleksbgbg <kozichka01@abv.bg>
2018-10-24 10:57:39 +0100

    [HttpCookie] Add model to store cookie data.

---
+++ Web Server/Http/Models/Cookies/HttpCookie.cs
@@ -0,0 +1,38 @@
+﻿namespace Http.Models.Cookies
+{
+    using System;
+
+    public class HttpCookie
+    {
+        private const int DefaultExpiryOffsetDays = 3;
+
+        public HttpCookie(string name, string value, int expireOffsetDays = DefaultExpiryOffsetDays)
+        {
+            Name = name;
+            Value = value;
+            ExpiresAt = DateTime.UtcNow.AddDays(expireOffsetDays);
+            IsNew = true;
+        }
+
+        public HttpCookie(string name, string value, DateTime expiresAt, bool isNew)
+        {
+            Name = name;
+            Value = value;
+            ExpiresAt = expiresAt;
+            IsNew = isNew;
+        }
+
+        public string Name { get; }
+
+        public string Value { get; }
+
+        public DateTime ExpiresAt { get; }
+
+        public bool IsNew { get; }
+
+        public override string ToString()
+        {
+            return $"{Name}={Value}; Expires={ExpiresAt.ToLongTimeString()}";
+        }
+    }
+}
