5908e43b7f6e089361dcdf72e6bcd0c33a4410b0
Aleksbgbg <kozichka01@abv.bg>
2018-10-26 10:21:25 +0100

    [Program] Implement controller registration using reflection.

--- Web Server/Demo/Program.cs
+++ Web Server/Demo/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace Demo
 {
+    using System;
+    using System.Linq;
+    using System.Reflection;
     using System.Threading.Tasks;
 
+    using Demo.Attributes;
     using Demo.Controllers;
 
     using Http.Models;
+    using Http.Models.Responses;
 
     using WebServer;
     using WebServer.Routing;
@@ -15,7 +20,43 @@
         {
             ServerRoutingTable serverRoutingTable = new ServerRoutingTable();
 
-            serverRoutingTable.RegisterOrOverwriteRoute(HttpRequestMethod.GET, "/", request => new HomeController().Index());
+            foreach (Type controllerType in Assembly.GetExecutingAssembly()
+                                                    .GetTypes()
+                                                    .Where(type => type.Namespace == "Demo.Controllers" && type.Name != "ControllerBase"))
+            {
+                foreach (MethodInfo method in controllerType.GetMethods())
+                {
+                    foreach (RouteAttribute routeAttribute in method.GetCustomAttributes<RouteAttribute>())
+                    {
+                        HttpRequestMethod requestMethod;
+
+                        switch (routeAttribute)
+                        {
+                            case HttpGetAttribute _:
+                                requestMethod = HttpRequestMethod.GET;
+                                break;
+
+                            case HttpPostAttribute _:
+                                requestMethod = HttpRequestMethod.POST;
+                                break;
+
+                            default:
+                                continue;
+                        }
+
+                        serverRoutingTable.RegisterOrOverwriteRoute(requestMethod,
+                                                                    routeAttribute.Route,
+                                                                    request =>
+                                                                    {
+                                                                        ControllerBase controller = (ControllerBase)Activator.CreateInstance(controllerType);
+
+                                                                        controller.Request = request;
+
+                                                                        return (IHttpResponse)method.Invoke(controller, null);
+                                                                    });
+                    }
+                }
+            }
 
             Server server = new Server(5000, serverRoutingTable);
 
