ff90b2d54ca2fd25367b045fd49b24332ef0b3f4
Aleksbgbg <kozichka01@abv.bg>
2018-10-26 13:31:50 +0100

    [FrameworkTest] Implement framework in test project.

---
+++ Web Server/FrameworkTest/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace FrameworkTest.Controllers
+{
+    using Framework.ActionResults;
+    using Framework.Controllers;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web Server/FrameworkTest/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FrameworkTest
+{
+    using System.Threading.Tasks;
+
+    using Framework;
+    using Framework.Routers;
+
+    using WebServer;
+
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            await MvcEngine.Run(new Server(8000, new ControllerRouter()));
+        }
+    }
+}
