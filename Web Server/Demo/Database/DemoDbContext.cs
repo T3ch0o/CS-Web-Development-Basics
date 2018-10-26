5fd727752738626392dddd5cb6d1a7032eead92f
Aleksbgbg <kozichka01@abv.bg>
2018-10-26 10:20:24 +0100

    [Migrations] Create database migrations and context from models.

---
+++ Web Server/Demo/Database/DemoDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace Demo.Database
+{
+    using Demo.Models;
+
+    using Microsoft.EntityFrameworkCore;
+
+    internal class DemoDbContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Track> Tracks { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=Demo;Integrated Security=True");
+        }
+    }
+}
