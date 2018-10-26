909630de4b163c7c68e7544c5e4eb933a2b32887
Aleksbgbg <kozichka01@abv.bg>
2018-10-26 13:30:54 +0100

    [IHttpHandler] Introduce interface with simple handle method.

---
+++ Web Server/WebServer/Api/IHttpHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace WebServer.Api
+{
+    using Http.Models.Requests;
+    using Http.Models.Responses;
+
+    public interface IHttpHandler
+    {
+        IHttpResponse Handle(IHttpRequest request);
+    }
+}
