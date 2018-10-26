cbc5a9fae306071f656a60ba6d5af43b42b163e7
Aleksbgbg <kozichka01@abv.bg>
2018-10-26 10:09:55 +0100

    [HttpCookie] Allow deletion, and improve ToString() formatting.

--- Web Server/Http/Models/Cookies/HttpCookie.cs
+++ Web Server/Http/Models/Cookies/HttpCookie.cs
@@ -24,15 +24,21 @@
 
         public string Name { get; }
 
-        public string Value { get; }
+        public string Value { get; private set; }
 
-        public DateTime ExpiresAt { get; }
+        public DateTime ExpiresAt { get; private set; }
 
         public bool IsNew { get; }
 
+        public void Delete()
+        {
+            ExpiresAt = DateTime.UtcNow.AddDays(-1);
+            Value = string.Empty;
+        }
+
         public override string ToString()
         {
-            return $"{Name}={Value}; Expires={ExpiresAt.ToLongTimeString()}";
+            return $"{Name}={Value}; Expires={ExpiresAt:R}; HttpOnly; Path=/";
         }
     }
 }
