b89594e3d2eb81c42cd9f6ebcaeb7e839619a73b
Aleksbgbg <kozichka01@abv.bg>
2018-10-28 14:05:18 +0000

    [IdentityUser] Add class which implements IIdentity.

---
+++ Web Server/Framework/Security/IdentityUser.cs
@@ -0,0 +1,29 @@
+﻿namespace Framework.Security
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed class IdentityUser : IdentityUser<string>
+    {
+        public IdentityUser()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+
+    public class IdentityUser<TKey> : IIdentity
+            where TKey : IEquatable<TKey>
+    {
+        public virtual TKey Id { get; protected set; }
+
+        public virtual string Username { get; set; }
+
+        public virtual string Password { get; set; }
+
+        public virtual string Email { get; set; }
+
+        public virtual bool IsValid { get; set; }
+
+        public virtual IEnumerable<string> Roles { get; set; }
+    }
+}
