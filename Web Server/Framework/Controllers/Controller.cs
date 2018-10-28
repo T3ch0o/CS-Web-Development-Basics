d16b7e5338d6cf59779d1dc99216a72373364de4
Aleksbgbg <kozichka01@abv.bg>
2018-10-28 14:11:19 +0000

    [Controller] Add functionality to ineroperate with new mechanisms in framework.

--- Web Server/Framework/Controllers/Controller.cs
+++ Web Server/Framework/Controllers/Controller.cs
@@ -1,28 +1,71 @@
 ﻿namespace Framework.Controllers
 {
+    using System;
+    using System.Collections.Generic;
     using System.Runtime.CompilerServices;
 
     using Framework.ActionResults;
-    using Framework.Utilities;
+    using Framework.Models;
+    using Framework.Security;
+    using Framework.Views;
 
     using Http.Models.Requests;
 
     public class Controller
     {
+        private IViewEngine _viewEngine;
+
+        private MvcContext _mvcContext;
+        public MvcContext MvcContext
+        {
+            protected get => _mvcContext;
+
+            set
+            {
+                _mvcContext = value;
+                _viewEngine = new ViewEngine(value, new ViewReader());
+            }
+        }
+
         public IHttpRequest Request { protected get; set; }
 
-        protected IViewable View([CallerMemberName] string caller = default)
+        public IIdentity Identity => (IIdentity)Request.Session.GetParameter("auth");
+
+        public Model ModelState { get; } = new Model();
+
+        protected IDictionary<string, object> PropertyBag { get; } = new Dictionary<string, object>();
+
+        protected IViewable View([CallerMemberName] string actionName = default)
         {
-            string controllerName = ControllerUtilities.GetControllerName(this);
+            string controllerName = GetType().Name.Replace(MvcContext.ControllersSuffix, string.Empty);
 
-            string viewPath = ControllerUtilities.GetActionViewPath(controllerName, caller);
+            string viewContent;
 
-            return new ViewResult(new View(viewPath));
+            try
+            {
+                viewContent = _viewEngine.RenderView(controllerName, actionName, PropertyBag);
+            }
+            catch (Exception e)
+            {
+                viewContent = _viewEngine.RenderError(e.Message);
+            }
+
+            return new ViewResult(new View(viewContent));
         }
 
         protected IRedirectable Redirect(string url)
         {
             return new RedirectResult(url);
+        }
+
+        protected void SignIn(IIdentity authorization)
+        {
+            Request.Session.AddParameter("auth", authorization);
+        }
+
+        protected void SignOut()
+        {
+            Request.Session.ClearParameters();
         }
     }
 }
