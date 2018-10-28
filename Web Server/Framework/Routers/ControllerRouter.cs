d009794b6b8b3b001346e780a27aadb9b3b2cf92
Aleksbgbg <kozichka01@abv.bg>
2018-10-28 14:10:59 +0000

    [ControllerRouter] Add parameter parsing, when controller methods accept parameters from request data.

--- Web Server/Framework/Routers/ControllerRouter.cs
+++ Web Server/Framework/Routers/ControllerRouter.cs
@@ -1,12 +1,16 @@
 ﻿namespace Framework.Routers
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
 
     using Framework.ActionResults;
     using Framework.Attributes;
+    using Framework.Attributes.Action;
+    using Framework.Attributes.Property;
     using Framework.Controllers;
+    using Framework.Extensions;
 
     using Http.Extensions;
     using Http.Models;
@@ -18,6 +22,15 @@
 
     public class ControllerRouter : IHttpHandler
     {
+        private readonly ResourceRouter _resourceRouter = new ResourceRouter();
+
+        private readonly MvcContext _mvcContext;
+
+        public ControllerRouter(MvcContext mvcContext)
+        {
+            _mvcContext = mvcContext;
+        }
+
         public IHttpResponse Handle(IHttpRequest request)
         {
             string[] pathParts = request.Path.ToLower().Split('/', StringSplitOptions.RemoveEmptyEntries);
@@ -25,9 +38,7 @@
             string controllerName = pathParts.First().Capitalize();
             string actionName = pathParts.Last().Capitalize();
 
-            MvcContext mvcContext = MvcContext.Instance;
-
-            string controllerAssemblyQualifiedName = string.Concat(mvcContext.AssemblyName, ".", mvcContext.ControllersNamespace, ".", controllerName, mvcContext.ControllersSuffix, ", ", mvcContext.AssemblyName);
+            string controllerAssemblyQualifiedName = string.Concat(_mvcContext.AssemblyName, ".", _mvcContext.ControllersNamespace, ".", controllerName, _mvcContext.ControllersSuffix, ", ", _mvcContext.AssemblyName);
 
             Type controllerType = Type.GetType(controllerAssemblyQualifiedName);
 
@@ -36,22 +47,50 @@
             if (targetMethod != null)
             {
                 Controller controller = (Controller)Activator.CreateInstance(controllerType);
-
                 controller.Request = request;
 
-                IActionResult actionResult = (IActionResult)targetMethod.Invoke(controller, null);
+                if (targetMethod.GetCustomAttributes<AuthorizeAttribute>()
+                                .Any(attribute => !attribute.IsAuthorized(controller.Identity)))
+                {
+                    return new UnauthorizedResult();
+                }
+
+                ParameterInfo[] parameters = targetMethod.GetParameters();
+                object[] arguments = new object[parameters.Length];
+
+                for (int parameterIndex = 0; parameterIndex < parameters.Length; parameterIndex++)
+                {
+                    ParameterInfo parameter = parameters[parameterIndex];
+
+                    Type parameterType = parameter.ParameterType;
+
+                    if (parameterType.IsPrimitiveOrString())
+                    {
+                        arguments[parameterIndex] = Convert.ChangeType(request.FormData[parameter.Name], parameterType);
+                    }
+                    else
+                    {
+                        arguments[parameterIndex] = InstantiateAndFill(parameterType, request.FormData);
+                    }
+
+                    controller.ModelState.IsValid = IsValidModel(arguments[parameterIndex]);
+                }
+
+                IActionResult actionResult = (IActionResult)targetMethod.Invoke(controller, arguments);
+
+                string result = actionResult.Invoke();
 
                 switch (actionResult)
                 {
-                    case IRedirectable redirectable:
-                        return new WebServer.Results.RedirectResult(redirectable.RedirectUrl);
+                    case IRedirectable _:
+                        return new WebServer.Results.RedirectResult(result);
 
-                    case IViewable viewable:
-                        return new HtmlResult(viewable.View.Render(), HttpStatusCode.OK);
+                    case IViewable _:
+                        return new HtmlResult(result, HttpStatusCode.OK);
                 }
             }
 
-            return new HttpResponse(HttpStatusCode.NotFound);
+            return _resourceRouter.Handle(request);
         }
 
         private static MethodInfo FindMethod(Type controllerType, string actionName, HttpRequestMethod requestMethod)
@@ -78,6 +117,43 @@
             }
 
             return null;
+        }
+
+        private static object InstantiateAndFill(Type objectType, IDictionary<string, object> formData)
+        {
+            object instance = Activator.CreateInstance(objectType);
+
+            foreach (PropertyInfo property in objectType.GetProperties())
+            {
+                if (property.PropertyType.IsPrimitiveOrString())
+                {
+                    property.SetValue(instance, Convert.ChangeType(formData[property.Name], property.PropertyType));
+                }
+                else
+                {
+                    property.SetValue(instance, InstantiateAndFill(property.PropertyType, formData));
+                }
+            }
+
+            return instance;
+        }
+
+        private static bool IsValidModel(object model)
+        {
+            foreach (PropertyInfo property in model.GetType().GetProperties())
+            {
+                ValidationAttribute[] validationAttributes = property.GetCustomAttributes<ValidationAttribute>().ToArray();
+
+                if (validationAttributes.Length > 0)
+                {
+                    if (validationAttributes.Any(validationAttribute => !validationAttribute.IsValid(property.GetValue(model))))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
