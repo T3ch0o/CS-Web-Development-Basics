aa33d76b4369d68fca8ae2e9d6691e316bcf5c01
Aleksbgbg <kozichka01@abv.bg>
2018-10-28 14:05:52 +0000

    [Model] Add class which has an 'IsValid' property which represents whether all properties are properly validated.

---
+++ Web Server/Framework/Models/Model.cs
@@ -0,0 +1,19 @@
+﻿namespace Framework.Models
+{
+    public class Model
+    {
+        private bool? _isValid;
+        public bool? IsValid
+        {
+            get => _isValid;
+
+            set
+            {
+                if (_isValid == null)
+                {
+                    _isValid = value;
+                }
+            }
+        }
+    }
+}
