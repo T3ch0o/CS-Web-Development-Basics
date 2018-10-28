b886697030f577b3292462ae7e058dc8c408835a
Aleksbgbg <kozichka01@abv.bg>
2018-10-28 14:13:14 +0000

    [WebHost] Add new starter class which manages dependency container and MvcContext.

---
+++ Web Server/Framework/WebHost.cs
@@ -0,0 +1,43 @@
+﻿namespace Framework
+{
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using Framework.Api;
+    using Framework.Dependency;
+    using Framework.Routers;
+
+    using WebServer;
+
+    public static class WebHost
+    {
+        private const int Port = 8000;
+
+        public static async Task Start(IMvcApplication mvcApplication)
+        {
+            MvcContext mvcContext = new MvcContext
+            {
+                AssemblyName = Assembly.GetEntryAssembly().GetName().Name,
+                ControllersNamespace = "Controllers",
+                ControllersSuffix = "Controller",
+                ViewsFolder = "Views",
+                ModelsFolder = "Models"
+            };
+
+            await Start(mvcApplication, mvcContext);
+        }
+
+        public static async Task Start(IMvcApplication mvcApplication, MvcContext mvcContext)
+        {
+            IDependencyContainer container = new DependencyContainer();
+            mvcApplication.ConfigureServices(container);
+
+            ControllerRouter controllerRouter = new ControllerRouter(mvcContext);
+            mvcApplication.Configure();
+
+            Server server = new Server(Port, controllerRouter);
+
+            await server.Run();
+        }
+    }
+}
