92b6bb13f5b9e215c4ad110173575484020ecde4
Aleksbgbg <kozichka01@abv.bg>
2018-10-28 14:03:23 +0000

    [UnauthorizedResult] Add result for 401 responses.

---
+++ Web Server/WebServer/Results/UnauthorizedResult.cs
@@ -0,0 +1,19 @@
+﻿namespace WebServer.Results
+{
+    using System.Text;
+
+    using Http.Models;
+    using Http.Models.Headers;
+    using Http.Models.Responses;
+
+    public class UnauthorizedResult : HttpResponse
+    {
+        private const string DefaultErrorHeading = "<h1>You do not have permission to access this functionality.</h1>";
+
+        public UnauthorizedResult() : base(HttpStatusCode.Unauthorized)
+        {
+            Headers.Add(new HttpHeader("Content-Type", "text/html"));
+            BodyBytes = Encoding.UTF8.GetBytes(DefaultErrorHeading);
+        }
+    }
+}
