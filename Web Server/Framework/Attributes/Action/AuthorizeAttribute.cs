4673ea08339f28d0658c94f96bd84bcb12ebc695
Aleksbgbg <kozichka01@abv.bg>
2018-10-28 14:06:31 +0000

    [AuthorizeAttribute] Add attribute to authorize users.

---
+++ Web Server/Framework/Attributes/Action/AuthorizeAttribute.cs
@@ -0,0 +1,32 @@
+﻿namespace Framework.Attributes.Action
+{
+    using System;
+    using System.Linq;
+
+    using Framework.Security;
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute
+    {
+        private readonly string _role;
+
+        public AuthorizeAttribute()
+        {
+        }
+
+        public AuthorizeAttribute(string role)
+        {
+            _role = role;
+        }
+
+        public bool IsAuthorized(IIdentity user)
+        {
+            if (_role == null)
+            {
+                return user != null;
+            }
+
+            return user.Roles.Contains(_role);
+        }
+    }
+}
