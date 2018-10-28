7b10f60ef1706b6e825882ca4b30a713dda986c2
Aleksbgbg <kozichka01@abv.bg>
2018-10-28 14:07:34 +0000

    [TypeExtensions] Add extensions to check if a time is primitive or string.

---
+++ Web Server/Framework/Extensions/TypeExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Framework.Extensions
+{
+    using System;
+
+    internal static class TypeExtensions
+    {
+        internal static bool IsPrimitiveOrString(this Type type)
+        {
+            return type.IsPrimitive || type == typeof(string);
+        }
+    }
+}
