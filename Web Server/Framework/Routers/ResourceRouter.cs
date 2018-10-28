3f4a1b1117c993564ad4ca895c82d4183065f44a
Aleksbgbg <kozichka01@abv.bg>
2018-10-28 14:09:40 +0000

    [ResourceRouter] Add router which manages static content.

---
+++ Web Server/Framework/Routers/ResourceRouter.cs
@@ -0,0 +1,26 @@
+﻿namespace Framework.Routers
+{
+    using System.IO;
+
+    using Http.Models;
+    using Http.Models.Requests;
+    using Http.Models.Responses;
+
+    using WebServer.Api;
+    using WebServer.Results;
+
+    public class ResourceRouter : IHttpHandler
+    {
+        public IHttpResponse Handle(IHttpRequest request)
+        {
+            string resourcePath = string.Concat("../Resources/", request.Path);
+
+            if (File.Exists(resourcePath))
+            {
+                return new InlineResourceResult(File.ReadAllBytes(resourcePath), HttpStatusCode.OK);
+            }
+
+            return new HttpResponse(HttpStatusCode.NotFound);
+        }
+    }
+}
