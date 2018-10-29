c7d65ba160149507ef163a06a1a1e020e9bb0d86
T3ch0o <trottimusbg@gmail.com>
2018-10-29 20:09:07 +0200

    [InlineResourceResult] Added new Header.

--- Web Server/WebServer/Results/InlineResourceResult.cs
+++ Web Server/WebServer/Results/InlineResourceResult.cs
@@ -10,6 +10,7 @@
         {
             Headers.Add(new HttpHeader("Content-Length", content.Length.ToString()));
             Headers.Add(new HttpHeader("Content-Disposition", "inline"));
+            Headers.Add("Content-Type", "text/css");
 
             BodyBytes = content;
         }
