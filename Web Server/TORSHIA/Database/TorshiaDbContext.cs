a905b6e6a2b72a0f63b4e8fde9f08c2695100e1f
Aleksbgbg <kozichka01@abv.bg>
2018-10-29 12:53:10 +0000

    [TorshiaDbContext] Add DbContext for models.

---
+++ Web Server/TORSHIA/Database/TorshiaDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace Torshia.Database
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Torshia.Models;
+
+    internal class TorshiaDbContext : DbContext
+    {
+        public DbSet<Report> Reports { get; set; }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=Torshia;Integrated Security=True")
+                          .UseLazyLoadingProxies();
+        }
+    }
+}
