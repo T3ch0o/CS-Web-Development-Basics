33e22bceac227d7fc7ec66075ba1d27345cdc8a5
T3ch0o <trottimusbg@gmail.com>
2018-10-29 15:03:13 +0200

    [HomeController] Added Index action.

---
+++ Web Server/TORSHIA/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace Torshia.Controllers
+{
+    using Framework.ActionResults;
+    using Framework.Controllers;
+
+    internal class HomeController : Controller
+    {
+        IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
